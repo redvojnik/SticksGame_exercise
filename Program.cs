af6718bf40ffe6d52ae3965efeb5567a51b038eb
redvojnik <igrkanal3@gmail.com>
2022-01-18 19:55:36 +0300

    Adding Files of SticksGame

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SticksGame
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input desired amount of sticks to play. Default value is << 10 >>.\n" +
+                              "You must input date from << 10 >> to << 100 >> Your date is ");
+            int desiredValueOfSticks = PrintDesiredAmountSticks();
+            var sticksGame = new SticksGame(desiredValueOfSticks);
+            sticksGame.RegistGameHandler(PrintMessage);
+            //создаем цикл для игры в палочки. После каждого
+            while (sticksGame.gameStatus == SticksGameStatus.IsStarted)
+            {
+                Console.WriteLine("\n\nPlease input number of sticks from 1 to 3. Your number is ");
+                int userSticksNumber = sticksGame.InputAmountSticks();
+                sticksGame.Take(userSticksNumber);
+                sticksGame.RegistGameHandler(PrintMessage);
+                sticksGame.CheckGameStatus();
+                sticksGame.ComputerTake();
+                sticksGame.RegistGameHandler(PrintMessage);
+                sticksGame.CheckGameStatus();
+            }
+            Console.Read();
+
+           static void PrintMessage (string message) => Console.WriteLine(message);
+            // метод для общего количества палочек, от уменьшения которого будет идти игра. Проивзодим проверку на корректность ввода числа
+           static int PrintDesiredAmountSticks ()
+           {
+                int value = int.Parse(Console.ReadLine());
+                if (value >= 10 && value <= 100)
+                    return value;
+                else
+                {
+                    Console.Write("\nYou put incorrect number. Try again!\n" +
+                                      "Your date is ");
+                    return PrintDesiredAmountSticks();
+                }
+           }        
+        }
+    }
+}
---
+++ SticksGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SticksGame
+{
+    public delegate void GameHandler(string message);
+    class SticksGame
+    {
+        public SticksGameStatus gameStatus = SticksGameStatus.IsStarted;
+        private int AmountSticks = 10;
+        GameHandler taken;
+        public void RegistGameHandler(GameHandler currentDelegate) => this.taken = currentDelegate;
+        public SticksGame(int AmountSticks) => this.AmountSticks = AmountSticks;
+        public void ComputerTake()
+        {
+                Random random = new();
+                int amountSticks = random.Next(1, this.AmountSticks < 3
+                                                  ? this.AmountSticks
+                                                  : 3);
+                this.AmountSticks -= amountSticks;
+                taken?.Invoke($"Computer made its move. It took <<{amountSticks}>> sticks.\n" +
+                              $"There are << {this.AmountSticks} >> sticks left.\n");
+        }
+        public void Take(int amountSticks)
+        {
+                AmountSticks -= amountSticks;
+                taken?.Invoke($"You took << {amountSticks} >> sticks.\n" +
+                              $"There are << {AmountSticks} >> sticks left!\n");
+        }
+        public int InputAmountSticks()
+        {
+            int value = int.Parse(Console.ReadLine());
+            if (value > 0 && value < 4 && value <= AmountSticks)
+                return value;
+            else
+            {
+                Console.Write("\n\nYou put incorrect number. Try again!\n" +
+                                  "Your number is ");
+                return InputAmountSticks();
+            }
+        }
+        public void CheckGameStatus()
+        {
+            if (AmountSticks == 0)
+            {
+                gameStatus = SticksGameStatus.IsEnded;
+                Console.WriteLine("The game is ended.");
+            }
+        }
+    }
+}
